e296e7b6f215a88b23826e27aaa24088b6233c76
JanTomassi <tomassijan@gmail.com>
2023-05-03 02:58:19 +0200

    moved gitignore in subfolder added color and style
    because the root .gitignore file was not allowing all the .xml file to be
    uploaded I have moved it in the docs folder to allow all the android
    .xml file to be uploaded
    
    added all the missing .xml file from the initial setup
    
    added the basic palette and not all the light theme

---
+++ implementation/mangaCheck/app/src/main/java/it/unitn/DISI/LPSMT/G03/mangacheck/MainActivity.kt
@@ -0,0 +1,8 @@
+package it.unitn.DISI.LPSMT.G03.mangacheck
+
+import androidx.appcompat.app.AppCompatActivity
+import it.unitn.DISI.LPSMT.G03.mangacheck.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+}
