10c558229965d97cf11b1a4db135c991096d9b2c
JanTomassi <tomassijan@gmail.com>
2023-05-16 04:24:16 +0200

    First basic fragment
    Assets:
    added some dimensions and some string
    removed the night theme for now it's easy to implement in the feature
    
    Single drawable:
    - added the plus and minus button for the chapter number component the two button are in vector graphics
    - made the generic text input for all the form
    - made the generic button for all the form of the app
    - added the basic surface common to all the object in overlay
    
    Layout:
    - created the layout 'add comics name'
    - setup of the main activity to support fragment
    
    Build script:
    - added the fragment library

--- implementation/mangaCheck/app/src/main/java/it/unitn/DISI/LPSMT/G03/mangacheck/MainActivity.kt
+++ implementation/mangaCheck/app/src/main/java/it/unitn/DISI/LPSMT/G03/mangacheck/MainActivity.kt
@@ -1,8 +1,16 @@
 package it.unitn.DISI.LPSMT.G03.mangacheck
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import it.unitn.DISI.LPSMT.G03.mangacheck.databinding.ActivityMainBinding
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<AddComicsFragment>(R.id.fragment_container_view)
+        }
+    }
 }
