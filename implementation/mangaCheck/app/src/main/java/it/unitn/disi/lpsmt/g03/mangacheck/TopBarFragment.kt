570005fb9e4326b1da0c9f05e4f7be8d1354fa70
elblasco <alessio.blascovich@studenti.unitn.it>
2023-05-20 17:19:50 +0200

    Implemented top_bar.xml
    Added the hamburger icon and a dummy Fragment `TopBarFragment` for the display test

---
+++ implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/TopBarFragment.kt
@@ -0,0 +1,26 @@
+package it.unitn.disi.lpsmt.g03.mangacheck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import it.unitn.disi.lpsmt.g03.mangacheck.databinding.TopBarLayoutBinding
+
+class TopBarFragment : Fragment() {
+    private var _binding: TopBarLayoutBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = TopBarLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
