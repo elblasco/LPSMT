111e80339e7aa02afab75627519497863e425caf
elblasco <alessio.blascovich@studenti.unitn.it>
2023-05-20 13:54:01 +0200

    Clicking on the button the file drower opens up
    The MIME type is "application/x-cbz", next step implementing off button on empty form

--- implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/AddComicsFragment.kt
+++ implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/AddComicsFragment.kt
@@ -1,17 +1,13 @@
 package it.unitn.disi.lpsmt.g03.mangacheck
 
-import android.app.Activity
-import android.content.ContentResolver.MimeTypeInfo
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.MimeTypeMap
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import androidx.core.graphics.drawable.toIcon
 import androidx.fragment.app.Fragment
 import it.unitn.disi.lpsmt.g03.mangacheck.databinding.AddComicsLayoutBinding
 
@@ -22,7 +18,11 @@ class AddComicsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = AddComicsLayoutBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -32,39 +32,23 @@ class AddComicsFragment : Fragment() {
         _binding = null
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
         super.onViewCreated(view, savedInstanceState)
+
         val addButton : Button = binding.submitButton
+        val nameText : EditText = binding.comicName
+        val chapterSelector : EditText = binding.chapterSelector.chapterNumber
+
         addButton.setOnClickListener {
-            // Code here executes on main thread after user presses button
-            //Toast.makeText(this.context, "Premuto il pulsanton", Toast.LENGTH_SHORT).show()
-            fileManagerRequest()
+            val fileExplorerIntent: Intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "application/x-cbz"
+                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+            }
+            startActivity(Intent.createChooser(fileExplorerIntent, "Pick a file"))
         }
-    }
 
-    //Custom functions definition
-
-    private fun fileManagerRequest(){
-        val fileExplorerIntent: Intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-            type = "*/*"
-            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
-        }
-        Toast.makeText(this.context, fileExplorerIntent.type, Toast.LENGTH_SHORT).show()
-        //startActivity(Intent.createChooser(fileExplorerIntent, "Pick a file"))
-        startActivityForResult(Intent.createChooser(fileExplorerIntent, "Pick a file"), 111)
-        /*fileExplorerIntent.data?.let {Uri->
-            MimeTypeMap.getFileExtensionFromUrl(Uri.toString())
-        }
-        Log.e("Dio merda", fileExplorerIntent.data.toString())*/
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 111 && resultCode == Activity.RESULT_OK){
-            val selectedFile = (data?.data).toString() // The URI with the location of the file
-            Log.e("A dire il vero tutto ok",selectedFile)
-            val mimeType = MimeTypeMap.getFileExtensionFromUrl(selectedFile)
-            Log.e("A dire il vero tutto ok",mimeType)
-        }
     }
 }
