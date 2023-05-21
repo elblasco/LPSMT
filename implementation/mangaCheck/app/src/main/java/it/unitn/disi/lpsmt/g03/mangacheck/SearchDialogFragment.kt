36b08503fd62f51876b352dc851073a273c4a92a
elblasco <alessio.blascovich@studenti.unitn.it>
2023-05-21 14:47:50 +0200

    Added `search_dialog.xml` the search index dialog
    Created a dummy Fragment for display `SearchDialogFragment.kt`, a new
    string named `search_hint` and create d a new drawable
    `search_icon_index_search.xml` for upsacling the search icon

---
+++ implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/SearchDialogFragment.kt
@@ -0,0 +1,24 @@
+package it.unitn.disi.lpsmt.g03.mangacheck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import it.unitn.disi.lpsmt.g03.mangacheck.databinding.SearchDialogBinding
+
+class SearchDialogFragment : Fragment() {
+    private var _binding: SearchDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = SearchDialogBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+}
