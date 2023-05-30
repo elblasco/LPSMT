57beeb2e21fd740e10ec34caa51cfe6c2af3117a
elblasco <alessio.blascovich@studenti.unitn.it>
2023-05-30 18:55:49 +0200

    Finished the `AdReadingByNameFragment`
    Completed the server request on coroutine and the following parsing an the UI update.
    Created a new action in the nav_graph that can carry data from AddReadingByName and AddReadingSetStatus.
    Update build.gradle to support type safe delivery of data between Fragment.

--- implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/add_reading/by_name/AddReadingByNameFragment.kt
+++ implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/add_reading/by_name/AddReadingByNameFragment.kt
@@ -1,6 +1,5 @@
 package it.unitn.disi.lpsmt.g03.mangacheck.add_reading.by_name
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,40 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ScrollView
-import android.widget.TextView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.websocket.WebSockets
-import io.ktor.client.request.HttpResponseData
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
-import io.ktor.http.ContentType
-import io.ktor.http.HttpMethod
-import io.ktor.http.charset
-import io.ktor.http.contentType
-import io.ktor.http.encodedPath
 import io.ktor.http.path
 import it.unitn.disi.lpsmt.g03.mangacheck.R
 import it.unitn.disi.lpsmt.g03.mangacheck.add_reading.by_name.data.ReadingByNameAdapter
 import it.unitn.disi.lpsmt.g03.mangacheck.databinding.AddReadingSelectByNameBinding
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
+import java.net.ConnectException
 
 class AddReadingByNameFragment : Fragment(R.layout.add_reading_select_by_name) {
     private var _binding: AddReadingSelectByNameBinding? = null
 
     private lateinit var searchButton: Button
     private lateinit var textBox: EditText
-    private lateinit var scrollView: ScrollView
+    private lateinit var linearLayout: LinearLayout
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -61,10 +49,12 @@ class AddReadingByNameFragment : Fragment(R.layout.add_reading_select_by_name) {
 
         searchButton = binding.submitButton
         textBox = binding.comicName
-        scrollView = binding.listsResultsQueryByName
+        linearLayout = binding.listsResultsQueryByName
 
         searchButton.setOnClickListener {
-            scrollView.post{ queryServerAnUpdateUI() }
+            // This trigger a waterfall of function
+            // queryServerAndUpdateUI -> parsing -> updateUI
+            linearLayout.post { queryServerAndUpdateUI() }
         }
     }
 
@@ -73,53 +63,79 @@ class AddReadingByNameFragment : Fragment(R.layout.add_reading_select_by_name) {
         _binding = null
     }
 
-    private fun queryServerAnUpdateUI() {
+    // Make a coroutine to query the server for all the manga that has the text
+    // in the TextBox in the name.
+    // If everything go all right it calls the parsing then the updateUI.
+    // If the server return a state not in 200 <= state <= 299 it create a
+    // toast with the error code.
+    // If the server is unreachable it catches the exception and produce a toast.
+    private fun queryServerAndUpdateUI() {
         val client: HttpClient = HttpClient()
         lateinit var formattedResponse: Array<Array<String>>
         val scope = CoroutineScope(Dispatchers.IO)
         val ipAddr: String = this.requireContext().getString(R.string.ip_addr)
         val serverPort: Int = this.requireContext().getString(R.string.server_port).toInt()
         scope.launch {
-            Log.v("Server side", "Sent request")
-            val response: HttpResponse = client.get {
-                url {
-                    host = ipAddr
-                    port = serverPort
-                    path("search/${textBox.text}")
+            try {
+                val response: HttpResponse = client.get {
+                    url {
+                        host = ipAddr
+                        port = serverPort
+                        path("search/${textBox.text}")
+                    }
                 }
-            }
-            if (response.status.value in 200..299) {
-                Log.v("Server side", response.status.value.toString())
-                formattedResponse = parsing(response.body())
-                updateUI(formattedResponse)
-            } else {
+                if (response.status.value in 200..299) {
+                    formattedResponse = parsing(response.body())
+                    updateUI(formattedResponse)
+                } else {
+                    withContext(Dispatchers.Main) {
+                        toaster("Error ${response.status.value}")
+                    }
+                }
+
+            } catch (e: ConnectException) {
                 withContext(Dispatchers.Main) {
-                    Toast.makeText(context, "Error ${response.status.value}", Toast.LENGTH_SHORT)
-                        .show()
+                    toaster("Connection Refused")
                 }
             }
         }
     }
 
+    // If the response is empty it creates a dummy button with ID -1 and an error as a text
     private fun updateUI(response: Array<Array<String>>) {
-        Runnable {
-            Log.v("Runnable","Inside Runnable")
-            response.forEachIndexed { index, internalArray ->
-                val comicDapter = ReadingByNameAdapter(
-                    internalArray[1],
+        linearLayout.post(Runnable {
+            linearLayout.removeAllViews()
+            if (response.isNotEmpty()) {
+                response.forEachIndexed { index, internalArray ->
+                    val comicEntry = ReadingByNameAdapter(
+                        internalArray[1],
+                        this@AddReadingByNameFragment.requireContext()
+                    )
+                    linearLayout.addView(comicEntry.getView(internalArray[0].toInt(), null, null))
+                }
+            } else {
+                val comicEntry = ReadingByNameAdapter(
+                    "Manga doesn't exist",
                     this@AddReadingByNameFragment.requireContext()
                 )
-                scrollView.addView(comicDapter.getView(index, null, null))
+                linearLayout.addView(comicEntry.getView(-1, null, null))
+                toaster("Manga doesn't exist")
             }
-        }
+        })
     }
 
+    // Give the response string of the query it divides it in a matrix of string.
+    // Given a row x in [x][0] we have the string containing the manga id
+    // and in [x][1] the name of the manga.
     companion object {
         private fun parsing(response: String): Array<Array<String>> {
             val regex = Regex(
-                """\[((?:(\d+),?|("[^,]+?"),?)+)]"""
+                """\[((?:(\d+),?|("[^,]+?"),?)+)]""" // Jan goes brrrrrrr
             )
-            val matches = regex.findAll(response)
+            val matches: Sequence<MatchResult> = regex.findAll(response)
+            if (matches.toList().isEmpty()) {
+                return Array(0) { arrayOf("", "") }
+            }
             val stringOfRegexGroups: String = matches.map {
                 it.groupValues[1]
             }.joinToString()
@@ -129,14 +145,18 @@ class AddReadingByNameFragment : Fragment(R.layout.add_reading_select_by_name) {
             }
             var indexOfFormattedResponse: Int = 0
             for (index in listOfValues.indices step 2) {
-                formattedResponse[indexOfFormattedResponse][0] = listOfValues[index] //id
-                formattedResponse[indexOfFormattedResponse][1] = listOfValues[index + 1] //name
+                formattedResponse[indexOfFormattedResponse][0] =
+                    listOfValues[index].removePrefix(" ") //id
+                formattedResponse[indexOfFormattedResponse][1] =
+                    listOfValues[index + 1].removeSurrounding("\"") //name
                 indexOfFormattedResponse += 1
-            }
-            for ( index in formattedResponse.indices){
-                Log.v(index.toString(), formattedResponse[index][1])
             }
             return formattedResponse
         }
+    }
+
+    // Prepare a delicious Toast for you
+    private fun toaster(msg : String) : Unit{
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
 }
