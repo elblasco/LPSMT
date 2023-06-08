766faff138fb1399bece98670ce6cd0d3a34a59a
elblasco <alessio.blascovich@studenti.unitn.it>
2023-06-08 10:29:34 +0200

    Fixed major bug on import an empty list.
    Added check before the action declaration to prevent exception.
    Removed all the useless permissions from the manifest.

--- implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/export/ExportFragment.kt
+++ implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/export/ExportFragment.kt
@@ -2,6 +2,7 @@ package it.unitn.disi.lpsmt.g03.mangacheck.export
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import it.unitn.disi.lpsmt.g03.mangacheck.R
@@ -13,21 +14,31 @@ class ExportFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val action =
-            registerForActivityResult(ActivityResultContracts.CreateDocument("text/xml")) {
-                writeOnFile(it)
-                requireActivity().supportFragmentManager.popBackStack()
-            }
-        action.launch("readingList")
-    }
-
-    private fun writeOnFile(uri: Uri?) {
         val fileReadingListPrivate = File(requireContext().filesDir, requireContext().getString(R.string.XML_file))
-        val pathReadingListPrivate : Path = fileReadingListPrivate.toPath()
-        val cursor = requireContext().contentResolver.openOutputStream(uri!!)
-        Files.copy(pathReadingListPrivate, cursor)
-        cursor?.flush()
-        cursor?.close()
+        if(fileReadingListPrivate.exists()) {
+            val action =
+                registerForActivityResult(ActivityResultContracts.CreateDocument("text/xml")) {
+                    writeOnFile(it, fileReadingListPrivate)
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            action.launch("readingList")
+        }
+        else{
+            requireActivity().supportFragmentManager.popBackStack()
+            toaster("Reading list doesn't exist")
+        }
     }
 
+    private fun writeOnFile(uri: Uri?, fileReadingListPrivate : File) {
+            val pathReadingListPrivate : Path = fileReadingListPrivate.toPath()
+            val cursor = requireContext().contentResolver.openOutputStream(uri!!)
+            Files.copy(pathReadingListPrivate, cursor)
+            cursor?.flush()
+            cursor?.close()
+    }
+
+    // Prepare a delicious Toast for you
+    private fun toaster(msg: String) {
+        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+    }
 }
