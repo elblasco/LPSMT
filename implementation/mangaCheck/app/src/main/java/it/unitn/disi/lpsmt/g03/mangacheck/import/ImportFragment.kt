d57d7f724a8786be798d2b7e13f04ee398e3d4fc
elblasco <alessio.blascovich@studenti.unitn.it>
2023-06-08 14:37:45 +0200

    Fixed bug on broken popBack and null uri.

--- implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/import/ImportFragment.kt
+++ implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/import/ImportFragment.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import it.unitn.disi.lpsmt.g03.mangacheck.R
 import java.io.File
 import java.nio.file.Files
@@ -16,16 +17,18 @@ class ImportFragment : Fragment(){
         val action =
             registerForActivityResult(ActivityResultContracts.GetContent()) {
                 importOnFile(it)
-                requireActivity().supportFragmentManager.popBackStack()
+                findNavController().popBackStack()
             }
         action.launch("""text/xml""")
     }
 
     private fun importOnFile(uri : Uri?){
-        val fileReadingListPrivate = File(requireContext().filesDir, requireContext().getString(R.string.XML_file))
-        val pathReadingListPrivate : Path = fileReadingListPrivate.toPath()
-        val cursor = requireContext().contentResolver.openInputStream(uri!!)
-        Files.copy(cursor, pathReadingListPrivate, StandardCopyOption.REPLACE_EXISTING)
-        cursor?.close()
+        if (uri != null){
+            val fileReadingListPrivate = File(requireContext().filesDir, requireContext().getString(R.string.XML_file))
+            val pathReadingListPrivate : Path = fileReadingListPrivate.toPath()
+            val cursor = requireContext().contentResolver.openInputStream(uri)
+            Files.copy(cursor, pathReadingListPrivate, StandardCopyOption.REPLACE_EXISTING)
+            cursor?.close()
+        }
     }
 }
