f52d47ea60876a5bdce8706bbd1b2b759aca75b3
JanTomassi <tomassijan@gmail.com>
2023-06-09 15:34:31 +0200

    Implemented the inflater
    Signed-off-by: Jan Tomassi <tomassijan@gmail.com>

--- implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/list_comic/ListComicFragment.kt
+++ implementation/mangaCheck/app/src/main/java/it/unitn/disi/lpsmt/g03/mangacheck/list_comic/ListComicFragment.kt
@@ -1,6 +1,29 @@
 package it.unitn.disi.lpsmt.g03.mangacheck.list_comic
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import it.unitn.disi.lpsmt.g03.mangacheck.databinding.ListComicLayoutBinding
 
 class ListComicFragment : Fragment() {
+    private var _binding: ListComicLayoutBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = ListComicLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
