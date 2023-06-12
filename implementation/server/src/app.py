20688ecc9e58df55c86d05e13a3de2bd91a1f38a
Jan Tomassi <41572782+JanTomassi@users.noreply.github.com>
2023-06-12 10:39:37 +0200

    Update app.py

--- implementation/server/src/app.py
+++ implementation/server/src/app.py
@@ -4,6 +4,7 @@ import re
 import sys
 import requests
 import base64
+from flask import Response
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ app = Flask(__name__)
 def query_for_comics(title: str):
     def regexp(y, x, search=re.search):
         return 1 if search(y, x) else 0
-    
+
     con = sqlite3.connect("../db/mangaCheck.db")
 
     con.create_function('regexp', 2, regexp)
@@ -20,13 +21,13 @@ def query_for_comics(title: str):
 
     res = cur.execute("SELECT id,title from manga WHERE lower(title) REGEXP ?", [r'(?i)^{}.*'.format(title)]).fetchall()
 
-    return res
+    return str(res)
 
 @app.route('/image/<id>')
 def query_for_image(id: str):
     def regexp(y, x, search=re.search):
         return 1 if search(y, x) else 0
-    
+
     con = sqlite3.connect("../db/mangaCheck.db")
 
     con.create_function('regexp', 2, regexp)
@@ -44,7 +45,25 @@ def query_for_image(id: str):
         image_url
     )
 
+    response = base64.b64encode(request_bitmap.content)
+
     if(request_bitmap.status_code not in range(200,300)):
         return('',404)
     else:
-        return base64.b64encode(request_bitmap.content)
+        return Response(request_bitmap.content,200,mimetype='image/jpeg')
+
+@app.route('/description/<id>')
+def query_for_description(id: str):
+    def regexp(y, x, search=re.search):
+        return 1 if search(y, x) else 0
+
+    con = sqlite3.connect("../db/mangaCheck.db")
+
+    con.create_function('regexp', 2, regexp)
+
+    cur = con.cursor()
+
+    res = cur.execute("SELECT description FROM manga WHERE id = ?", [id]).fetchall()
+
+    # Return the first 100 characters followed by 3 dots
+    return str(res)[3:103]+"..."
