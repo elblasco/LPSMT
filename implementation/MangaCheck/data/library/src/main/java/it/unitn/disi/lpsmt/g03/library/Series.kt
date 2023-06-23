d823a4735205746b2bcb48b60a74dbefdfea2f74
JanTomassi <tomassijan@gmail.com>
2023-06-23 13:31:50 +0200

    Made Series a Parcelable
    Signed-off-by: JanTomassi <tomassijan@gmail.com>

--- implementation/MangaCheck/data/library/src/main/java/it/unitn/disi/lpsmt/g03/library/Series.kt
+++ implementation/MangaCheck/data/library/src/main/java/it/unitn/disi/lpsmt/g03/library/Series.kt
@@ -1,6 +1,8 @@
 package it.unitn.disi.lpsmt.g03.library
 
 import android.net.Uri
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
@@ -17,4 +19,36 @@ data class Series(
     @ColumnInfo("image url") val imageUri: Uri?,
     @ColumnInfo("is one-shot") val isOne_shot: Boolean,
     @ColumnInfo("lastAccess") val lastAccess: Date
-)
+) : Parcelable {
+    constructor(source: Parcel) : this(
+        source.readInt(),
+        source.readString()!!,
+        source.readString()?.let { ReadingState.valueOf(it) }!!,
+        source.readString(),
+        source.readInt(),
+        source.readString()?.let { Uri.parse(it) },
+        source.readInt() > 0,
+        source.readString()?.let { Date.valueOf(it) }!!
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeInt(uid)
+        writeString(title)
+        writeString(status.name)
+        writeString(description)
+        writeInt(chapters ?: 0)
+        writeString(imageUri.toString())
+        writeInt(if (isOne_shot) 1 else 0)
+        writeString(lastAccess.toString())
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Series> = object : Parcelable.Creator<Series> {
+            override fun createFromParcel(source: Parcel): Series = Series(source)
+            override fun newArray(size: Int): Array<Series?> = arrayOfNulls(size)
+        }
+    }
+}
