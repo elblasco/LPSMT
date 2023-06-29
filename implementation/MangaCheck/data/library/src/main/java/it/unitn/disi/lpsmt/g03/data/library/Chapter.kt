a891acb31af8ee40ed7ec9b3b0990f2290cf6939
JanTomassi <tomassijan@gmail.com>
2023-06-29 02:06:15 +0200

    Moved order of columns
    Signed-off-by: JanTomassi <tomassijan@gmail.com>

--- implementation/MangaCheck/data/library/src/main/java/it/unitn/disi/lpsmt/g03/data/library/Chapter.kt
+++ implementation/MangaCheck/data/library/src/main/java/it/unitn/disi/lpsmt/g03/data/library/Chapter.kt
@@ -11,11 +11,11 @@ import java.time.ZonedDateTime
 @Entity(foreignKeys = [ForeignKey(entity = Series::class,
     parentColumns = ["uid"],
     childColumns = ["seriesId"])])
-data class Chapter(@PrimaryKey(autoGenerate = true) val uid: Long,
-    @ColumnInfo("seriesId") val seriesId: Long,
+data class Chapter(@ColumnInfo("seriesId") val seriesId: Long,
     @ColumnInfo("chapter_title") val chapter: String,
     @ColumnInfo("chapter_num") val chapterNum: Int,
     @ColumnInfo("current_page") val currentPage: Int,
     @ColumnInfo("state") val state: ReadingState,
     @ColumnInfo("comic_file") val file: Uri?,
-    @ColumnInfo("lastAccess") val lastAccess: ZonedDateTime)
+    @ColumnInfo("lastAccess") val lastAccess: ZonedDateTime,
+    @PrimaryKey(autoGenerate = true) val uid: Long = 0)
